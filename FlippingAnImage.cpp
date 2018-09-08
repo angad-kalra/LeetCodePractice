1fab98aaa94c08cbdbc11a9ca9a5ace509f56e55
angad-kalra <angadkalra07@gmail.com>
2018-09-08 15:32:54 -0700

    Create FlippingAnImage.cpp

---
+++ FlippingAnImage.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<vector<int>> flipAndInvertImage(vector<vector<int>>& A) {
+        for(auto& row:A)   {
+            reverse(row.begin(),row.end());
+            for(auto& x:row)   {
+                x^=1;
+            }
+        }
+        return A;
+    }
+};
